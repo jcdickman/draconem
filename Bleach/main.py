115b3bdc88e45fae1450f46def36d85e40c1f561
elliotjkd <elliotjkd@gmail.com>
2023-01-02 22:39:00 -0600

    running left animation

--- Bleach/main.py
+++ Bleach/main.py
@@ -4,13 +4,14 @@ WIDTH = 800
 HEIGHT = 600
 
 
-
+# Player Variables
 player = Actor("standing_still_1", center=(400,300))
 standing_animation_list = ["standing_still_1", "standing_still_2", "standing_still_3", "standing_still_4"]
 rra_list = ["run_right_1", "run_right_2", "run_right_3", "run_right_4", "run_right_5", "run_right_6"]
-lra_list = []
+lra_list = ["run_left_1", "run_left_2", "run_left_3", "run_left_4", "run_left_5", "run_left_6"]
 cur_standing_image = 0
 cur_right_image = 0
+cur_left_image = 0
 delay_buffer = 0
 delay_time = .10
 
@@ -22,9 +23,11 @@ delay_time = .10
 def getStandingImage():
     return standing_animation_list[cur_standing_image]
 
-def getRunningImage():
+def getRunningRightImage():
     return rra_list[cur_right_image]
 
+def getRunningLeftImage():
+    return lra_list[cur_left_image]
 
 
 
@@ -38,11 +41,17 @@ def playerStandingStill():
 
 def playerRunningRight():
     global cur_right_image
-    player.image = getRunningImage()
+    player.image = getRunningRightImage()
     cur_right_image += 1
     if cur_right_image >= len(rra_list):
         cur_right_image = 0
 
+def playerRunningLeft():
+    global cur_left_image
+    player.image = getRunningLeftImage()
+    cur_left_image += 1
+    if cur_left_image >= len(rra_list):
+        cur_left_image = 0
 
 
 
@@ -58,6 +67,7 @@ def update(dt):
     if delay_buffer >= delay_time:
         delay_buffer = 0
         if keyboard.left:
+            playerRunningLeft()
             player.x -= 20
         elif keyboard.right:
             playerRunningRight()
