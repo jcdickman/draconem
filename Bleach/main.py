b3c79f3c7bf1e05289a15ff803e3ef6e55380c0a
elliotjkd <elliotjkd@gmail.com>
2023-01-02 22:07:32 -0600

    running right animation and fixed running glitch

--- Bleach/main.py
+++ Bleach/main.py
@@ -8,10 +8,14 @@ HEIGHT = 600
 player = Actor("standing_still_1", center=(400,300))
 standing_animation_list = ["standing_still_1", "standing_still_2", "standing_still_3", "standing_still_4"]
 rra_list = ["run_right_1", "run_right_2", "run_right_3", "run_right_4", "run_right_5", "run_right_6"]
+lra_list = []
 cur_standing_image = 0
 cur_right_image = 0
 delay_buffer = 0
 delay_time = .10
+
+
+
 
 
 
@@ -54,10 +58,10 @@ def update(dt):
     if delay_buffer >= delay_time:
         delay_buffer = 0
         if keyboard.left:
-            player.x -= (2 * (1 / delay_time))
+            player.x -= 20
         elif keyboard.right:
             playerRunningRight()
-            player.x += (2 * (1 / delay_time))
+            player.x += 20
         else:
             playerStandingStill()
     else:
